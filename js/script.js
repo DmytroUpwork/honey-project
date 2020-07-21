71a074e68956eaadda4599cec8029dc1393ef1b6
Dmitrii <lomonovskij@gmail.com>
2020-07-21 17:18:27 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  //owl carousel ===================================================
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+
+
+
+});
