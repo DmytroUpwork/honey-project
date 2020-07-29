af743382e9119b7a50a2345b353c68755529ee83
Dmitrii <lomonovskij@gmail.com>
2020-07-29 09:26:30 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+
+
   //owl carousel ===================================================
   $(".owl-carousel").owlCarousel({
     loop: true,
@@ -21,3 +24,17 @@ $(document).ready(function () {
 
 
 });
+
+let $navbar_menu_toggler = document.querySelector(".button_menu"),
+  $navbar_menu_collapse = document.querySelector(".menu_1"),
+  $navbar_button_line = document.querySelectorAll(".button_menu span");
+
+$navbar_menu_toggler.onclick = menu_mobile_toggler;
+
+function menu_mobile_toggler(event) {
+  $navbar_menu_collapse.classList.toggle("hidden_menu_1");
+  $navbar_menu_toggler.classList.toggle("position_menu_button");
+  $navbar_button_line[0].classList.toggle("navbar_line_active_1");
+  $navbar_button_line[2].classList.toggle("navbar_line_active_2");
+  $navbar_button_line[1].classList.toggle("navbar_line_active_3");
+};
