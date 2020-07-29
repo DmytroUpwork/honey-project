92f91b73fbe49d98935d851a5eaccc3b30616708
Dmitrii <lomonovskij@gmail.com>
2020-07-29 12:36:59 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -27,9 +27,12 @@ $(document).ready(function () {
 
 let $navbar_menu_toggler = document.querySelector(".button_menu"),
   $navbar_menu_collapse = document.querySelector(".menu_1"),
+  $navbar_menu_mobile_toggler = document.querySelector(".navbar-toggler"),
+  $navbar_button_menu_mobile_line = document.querySelectorAll(".navbar-toggler span"),
   $navbar_button_line = document.querySelectorAll(".button_menu span");
 
 $navbar_menu_toggler.onclick = menu_mobile_toggler;
+$navbar_menu_mobile_toggler.onclick = navbar_button_menu_mobile_line;
 
 function menu_mobile_toggler(event) {
   $navbar_menu_collapse.classList.toggle("hidden_menu_1");
@@ -37,4 +40,10 @@ function menu_mobile_toggler(event) {
   $navbar_button_line[0].classList.toggle("navbar_line_active_1");
   $navbar_button_line[2].classList.toggle("navbar_line_active_2");
   $navbar_button_line[1].classList.toggle("navbar_line_active_3");
+};
+
+function navbar_button_menu_mobile_line(event) {
+  $navbar_button_menu_mobile_line[0].classList.toggle("navbar_line_mobile_active_1");
+  $navbar_button_menu_mobile_line[2].classList.toggle("navbar_line_mobile_active_2");
+  $navbar_button_menu_mobile_line[1].classList.toggle("navbar_line_mobile_active_3");
 };
